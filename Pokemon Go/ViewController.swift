65b6f4928ce35d280ea6655c5b9617eb1b485c80
Vinicius Rezende <vinicius.rezende@rethink.dev>
2023-01-12 11:22:57 -0300

    Initial Commit

---
+++ Pokemon Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon Go
+//
+//  Created by Vinicius Rezende on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
